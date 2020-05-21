673d44da0dbe153cf94a802ddb401c425f918911
Michael Bade <mbade314@gmail.com>
2020-05-21 19:08:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,4 +13,13 @@ class Transfer
   def valid?
     @sender.valid? && receiver.valid? ? true : false
   end
+
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    end
+  end
+  
 end
