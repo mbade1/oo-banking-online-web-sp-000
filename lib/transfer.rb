873804659af8aefd80e2a7ace3c55cae0163c7e6
Michael Bade <mbade314@gmail.com>
2020-05-21 21:11:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,19 +12,6 @@ class Transfer
     @sender.valid? && receiver.valid? ? true : false
   end
 
-  # def execute_transaction
-  #   if @sender.balance < @amount && @sender.status == "closed"
-  #     @status = "rejected"
-  #     "Transation rejected. Please check your account balance."
-  #   # elsif @status == "complete"
-  #   #   puts "Transaction was already executed."
-  #   else
-  #     @sender.balance -= @amount
-  #     @receiver.balance += @amount
-  #     @status = "complete"
-  #   end
-  # end
-
   def execute_transaction
     if (@sender.valid? && @receiver.valid?) && (@sender.balance > @amount && @sender.status = "pending")
       @sender.balance -= @amount
