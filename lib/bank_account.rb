751a2c0f03d7959ab196ac8bc7ff8a40cb5d8cd6
Michael Bade <mbade314@gmail.com>
2020-05-21 18:53:41 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
 class BankAccount
 
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def balance=(balance)
+    @balance
+  end
+
+  def status=(status)
+    @status
+  end
+
+  def valid?
+    (@status == "open") && (@balance > 0)
+  end
+
+  def close_account
+    if !valid?
+      @status = "closed"
+    end
+  end
+
 end
