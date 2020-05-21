02a0106704c7126e8700c5331d4ce2a7dbc77c75
Michael Bade <mbade314@gmail.com>
2020-05-21 18:57:05 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -26,7 +26,11 @@ class BankAccount
   end
 
   def valid?
-    (@status == "open") && (@balance > 0) ? true : false
+    if (@status == "open") && (@balance > 0) 
+      true
+    else
+      false
+    end
 
   end
 
