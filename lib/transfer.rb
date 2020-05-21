7b38dcc35d8ef5e8e8ddb5161269a6f83c770811
Michael Bade <mbade314@gmail.com>
2020-05-21 19:05:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :transfer_amount
 
 
-  def initialize
+  def initialize(sender, receiver, transfer_amount)
     @sender = sender
     @receiver = receiver
     @status = status
