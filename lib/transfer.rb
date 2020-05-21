064b1d09fa69afe14c2b4090bbe3a93d84e3be1d
Michael Bade <mbade314@gmail.com>
2020-05-21 19:09:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else 
+    else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
